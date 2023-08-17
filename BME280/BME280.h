636e741c8e268cab694654ce423851a0f09e570b
gdex <gdex1974@gmail.com>
2023-08-17 13:11:32 +0000

    Improve and fix BME280 driver

--- BME280/BME280.h
+++ BME280/BME280.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <cstdint>
+#include <array>
+#include <optional>
 
 namespace embedded
 {
 
 class I2CHelper;
+class PersistentStorage;
 
 class BMPE280
 {
@@ -36,19 +39,16 @@ public:
         , OversamplingX16
     };
 
-    /**
-    * Stand by time between measurements in normal mode
-    */
     enum class StandbyTime : uint8_t
     {
-        Standby500us = 0      /* stand by time 0.5ms */
-        , Standby62500us      /* stand by time 62.5ms */
-        , Standby125ms     /* stand by time 125ms */
-        , Standby250ms     /* stand by time 250ms */
-        , Standby500ms     /* stand by time 500ms */
-        , Standby1s    /* stand by time 1s */
-        , Standby2s    /* stand by time 2s BMP280, 10ms BME280 */
-        , Standby4s    /* stand by time 4s BMP280, 20ms BME280 */
+        Standby500us = 0
+        , Standby62500us
+        , Standby125ms
+        , Standby250ms
+        , Standby500ms
+        , Standby1s
+        , Standby2s
+        , Standby4s
     };
 
     explicit BMPE280(I2CHelper &device,
@@ -57,7 +57,7 @@ public:
                      SamplingRate temperatureSampling = SamplingRate::OversamplingX16,
                      SamplingRate humiditySampling = SamplingRate::OversamplingX16,
                      StandbyTime interval = StandbyTime::Standby250ms)
-            : device(device), filterCoeff(filter), oversamplingPressure(pressureSampling), oversamplingTemperature(
+            : device(device), filteringMode(filter), oversamplingPressure(pressureSampling), oversamplingTemperature(
             temperatureSampling), oversamplingHumidity(humiditySampling), standbyTime(interval)
     {
 
@@ -65,6 +65,8 @@ public:
 
     // returns zero if successful or a number of failed stage
     int init();
+    bool loadCalibrationData(PersistentStorage &storage, std::string_view name);
+    bool saveCalibrationData(PersistentStorage &storage, std::string_view name);
     bool reset();
 
     bool startMeasurement(bool continuous = false)
@@ -88,48 +90,46 @@ public:
     // relative humidity in Percents
     bool getMeasureData(float &temperature, float &pressure, float &humidity);
 
-    bool canMeasureHumidity() const { return id == bme280ChipId; }
+    bool canMeasureHumidity() const { return calibrationData.humidityCompensation.has_value(); }
 
 private:
     int32_t calculateFineTemperature(int32_t rawTemperature) const;
     uint32_t calculateFineHumidity(int32_t rawHumidity, int32_t fineTemperature) const;
     uint32_t calculateFinePressure(int32_t rawPressure, int32_t fineTemperature) const;
 
-    bool readTemperatureAndPressureCalibrationData();
+    bool readPTCalibrationData();
     bool readHumidityCalibrationData();
     bool setMeasurementMode(MeasurementMode mode);
 
     static constexpr uint8_t bmp280ChipId = 0x58;
     static constexpr uint8_t bme280ChipId = 0x60;
 
-    struct
+    struct CalibrationData
     {
-        uint16_t T1;
-        int16_t T2;
-        int16_t T3;
-    } temperatureCompensation{};
+        struct PTCompensationData
+        {
+            uint16_t compT1;
+            int16_t compT2;
+            int16_t compT3;
+            uint16_t compP1;
+            std::array<int16_t, 8> compP2to9;
+        } ptCompensation {};
 
-    struct
-    {
-        uint16_t P1;
-        int16_t POther[8];
-    } pressureCompensation{};
-
-    struct
-    {
-        int16_t H2;
-        int16_t H4;
-        int16_t H5;
-        uint8_t H1;
-        uint8_t H3;
-        int8_t H6;
-    } humidityCompensation{};
-
-    uint8_t id{};        /* Chip ID */
+        struct HumidityCompensationData
+        {
+            uint8_t compH1;
+            int16_t compH2;
+            uint8_t compH3;
+            int16_t compH4;
+            int16_t compH5;
+            int8_t compH6;
+        };
+        std::optional<HumidityCompensationData> humidityCompensation;
+    } calibrationData;
 
     I2CHelper &device;
 
-    FilteringMode filterCoeff;
+    FilteringMode filteringMode;
     SamplingRate oversamplingPressure;
     SamplingRate oversamplingTemperature;
     SamplingRate oversamplingHumidity;
