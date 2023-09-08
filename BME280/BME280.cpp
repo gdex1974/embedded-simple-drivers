97240d5e62611887768bc106b6f9c0a8ac481cc5
gdex <gdex1974@gmail.com>
2023-09-08 15:55:35 +0200

    Fix build after merge

--- BME280/BME280.cpp
+++ BME280/BME280.cpp
@@ -239,8 +239,8 @@ uint32_t BMPE280::calculateFineHumidity(const int32_t rawHumidity, const int32_t
             * (((((((value * humidityCompensation.compH6) >> 10) * (((value * humidityCompensation.compH3) >> 11) + 32768))
             >> 10) + 2097152) * humidityCompensation.compH2 + 8192) >> 14);
     value -= ((square(value >> 15) >> 7) * humidityCompensation.compH1) >> 4;
-    value = std::max(value, 0l);
-    value = std::min(value, 419430400l);
+    value = std::max(value, decltype(value)(0));
+    value = std::min(value, decltype(value)(419430400));
     return value >> 12;
 }
 
